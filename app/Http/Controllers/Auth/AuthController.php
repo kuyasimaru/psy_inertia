99f62e506b9ed3d5231ed1cb446847279cfebc18
iinuma_jinto <rfanzhao@gmail.com>
2024-07-24 08:48:58 +0900

    auth

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Tightenco\Ziggy\Ziggy;
+
+
+class AuthController extends Controller
+{
+    private ?string $role;
+    private string $package;
+
+    public function __construct() {
+        $this->role = detect_role();
+        $camel = Str::camel($this->role);
+        $camel[0] = strtoupper($camel[0]);
+        $this->package = empty($this->role) ? '' : $camel;
+    }
+
+    public function route($route): string
+    {
+        if (empty($this->role)) {
+            return $route;
+        }
+        return $this->role . '.' . $route;
+    }
+
+    public function resource($path): string
+    {
+        if (empty($this->role)) {
+            return $path;
+        }
+        return $this->package . '/' . $path;
+    }
+
+    public function modelName() {
+        if (empty($this->role)) {
+            return User::class;
+        }
+        return '\\App\\Models\\' . $this->package;
+    }
+}
