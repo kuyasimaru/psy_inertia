de789c27cbd8179129e290da15367078db8572bc
iinuma_jinto <rfanzhao@gmail.com>
2024-08-22 21:36:18 +0900

    gmail

--- app/Http/Controllers/AdminInvite.php
+++ app/Http/Controllers/AdminInvite.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use App\mail\MemberInvitMail;
+use App\Mail\MemberInvitMail;
 use App\Models\Member;
 use App\Models\Company;
 use App\Models\Invite;
