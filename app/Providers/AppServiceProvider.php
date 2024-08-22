7356ee60ca9d7f8065a49b37e12c48580248d051
iinuma_jinto <rfanzhao@gmail.com>
2024-08-22 22:38:45 +0900

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if(\App::environment(['production' || 'develop'])){
+        if(\App::environment(['production']) || \App::environment(['develop'])){
             \URL::forceScheme('https');
         }
     }
